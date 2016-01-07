020e4275a67bcb2eb739cd937e71d0036e34c851
Kassidy Douglas and Nathan Romike <nathanromike@gmail.com>
2016-01-07 11:38:04 -0800

    Add correct grammar, add Spark pages, recall input result.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 import java.util.Map;
 import java.util.HashMap;
+import javax.swing.JOptionPane;
 
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -16,10 +17,30 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/results", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/results.vtl" );
+
+      String userCoinString = request.queryParams("coinage");
+      model.put("userCoinString", userCoinString);
+
+      Integer looseChange = Integer.parseInt(userCoinString);
+
+      System.out.println(looseChange);
+
+      if (looseChange == null) {
+        JOptionPane.showMessageDialog (null, "Don't be a jerk. Put in an actual number, please.");
+      }
+
+      String finalResult = coinCombo(looseChange);
+      model.put("finalResult", finalResult);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 
-  public static String coinCombo(String userInput) {
-    Integer userCoins = Integer.parseInt(userInput);
+  public static String coinCombo(Integer userCoins) {
+
+    String output = "Here's what'cha got: ";
 
     Integer quarters = 0;
     Integer dimes = 0;
@@ -31,9 +52,26 @@ public class App {
       quarters++;
     }
 
+    if (quarters == 1) {
+      output += (quarters + " quarter, ");
+    } else if (quarters > 1) {
+      output += (quarters + " quarters, ");
+    } else {
+      output += " No quarters, ";
+    }
+
+
     while ( userCoins >= 10) {
       userCoins -= 10;
       dimes++;
+    }
+
+    if (dimes == 1) {
+      output += (dimes + " dime, ");
+    } else if (dimes > 1) {
+      output += (dimes + " dimes, ");
+    } else {
+      output += " no dimes, ";
     }
 
     while ( userCoins >= 5) {
@@ -41,11 +79,27 @@ public class App {
       nickels++;
     }
 
+    if (nickels == 1) {
+      output += (nickels + " nickel, ");
+    } else if (nickels > 1) {
+      output += (nickels + " nickels, ");
+    } else {
+      output += " no nickels, ";
+    }
+
     while ( userCoins > 0) {
       userCoins -= 1;
       pennies++;
     }
 
-    return String.format("Your change is %d quarters, %d dimes, %d nickels, and %d pennies", quarters, dimes, nickels, pennies);
+    if (pennies == 1) {
+      output += ("and " + pennies + " penny.");
+    } else if (pennies > 1) {
+      output += ("and " + pennies + " pennies.");
+    } else {
+      output += "and no pennies.";
+    }
+
+    return output;
   }
 }
