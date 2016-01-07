58981fe2f27f5da8f6a55d97aa72fcc8182771c2
Kassidy Douglas and Nathan Romike <nathanromike@gmail.com>
2016-01-07 09:47:59 -0800

    Change app to while statements, add test for pennies, pass unit tests

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,12 +22,30 @@ public class App {
     Integer userCoins = Integer.parseInt(userInput);
 
     Integer quarters = 0;
+    Integer dimes = 0;
+    Integer nickels = 0;
+    Integer pennies = 0;
 
-    while (userCoins > 24) {
+    while ( userCoins >= 25 ) {
       userCoins -= 25;
       quarters++;
     }
 
-    return String.format("Your change is %d quarters", quarters);
+    while ( userCoins >= 10) {
+      userCoins -= 10;
+      dimes++;
+    }
+
+    while ( userCoins >= 5) {
+      userCoins -= 5;
+      nickels++;
+    }
+
+    while ( userCoins > 0) {
+      userCoins -= 1;
+      pennies++;
+    }
+
+    return String.format("Your change is %d quarters, %d dimes, %d nickels, and %d pennies", quarters, dimes, nickels, pennies);
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,7 +6,28 @@ public class AppTest {
   @Test
   public void coinCombo_shouldGiveCoinCombo75Cents_3Quarters() {
     App app = new App();
-    String coinResult = "Your change is 3 quarters";
+    String coinResult = "Your change is 3 quarters, 0 dimes, 0 nickels, and 0 pennies";
     assertEquals(coinResult, app.coinCombo("75"));
+  }
+
+  @Test
+  public void coinCombo_shouldGiveCoinCombo85Cents_3Quarters1Dime() {
+    App app = new App();
+    String coinResult = "Your change is 3 quarters, 1 dimes, 0 nickels, and 0 pennies";
+    assertEquals(coinResult, app.coinCombo("85"));
+  }
+
+  @Test
+  public void coinCombo_shouldGiveCoinCombos90Cents_3Quarters1Dime1Nickel() {
+    App app = new App();
+    String coinResult = "Your change is 3 quarters, 1 dimes, 1 nickels, and 0 pennies";
+    assertEquals(coinResult, app.coinCombo("90"));
+  }
+
+  @Test
+  public void coinCombo_shouldGiveCoinCombos90Cents_3Quarters1Dime1Nickel3Pennies() {
+    App app = new App();
+    String coinResult = "Your change is 3 quarters, 1 dimes, 1 nickels, and 3 pennies";
+    assertEquals(coinResult, app.coinCombo("93"));
   }
 }
